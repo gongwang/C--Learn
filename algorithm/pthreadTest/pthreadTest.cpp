eadaf10c36edc1f7b82f5f77b9ab761d66e046a4
zhaomilu <reback1991@126.com>
2020-03-14 16:05:34 +0800

    增加排序算法 工程
    -------------------
    
    1.快速排序算法实现

--- algorithm/pthreadTest/pthreadTest.cpp
+++ algorithm/pthreadTest/pthreadTest.cpp
@@ -34,25 +34,89 @@ void * ConnectClientProc(void * args)
     return (void*)cRet; //or 
 }
 
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//
+//    char cName[32] = "cococo";
+//
+//    pthread_t ptid;
+//    pthread_create(&ptid,NULL,ConnectClientProc,(void*)cName);
+//    //pthread_detach(ptid);
+//
+//    printf("Main is running... \n");
+//
+//    void *cRcv;
+//    pthread_join(ptid, &cRcv);
+//    printf("Thread over, and recv : %d\n", (*(STRTEMP*)cRcv).i);
+//    delete cRcv;
+//    cRcv = NULL;
+//
+//    system("pause");
+//
+//	return 0;
+//}
+//
+////位移测试
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//
+//    char cName[4] = {0};
+//
+//    cName[3] = 65535&0xFF;
+//    cName[2] = 65535>>8 &0xFF;
+//    cName[1] = 65535>>16 &0xFF; 
+//    cName[0] = 65535>>24 &0xFF;
+//
+//    printf("%s\n",cName);
+//
+//    system("pause");
+//
+//    return 0;
+//}
+
+
+
+//静态局部变量测试
+
+class cStaticTest
+{
+public:
+    cStaticTest(){
+        static int i = 0;
+        i++;
+        printf("cur i = %d\n",i);
+    }
+protected:
+private:
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-    char cName[32] = "cococo";
+    cStaticTest *pTemp1 = new cStaticTest();
+    if (pTemp1)
+    {
+        delete pTemp1;
+        pTemp1 = NULL;
+    }
 
-    pthread_t ptid;
-    pthread_create(&ptid,NULL,ConnectClientProc,(void*)cName);
-    //pthread_detach(ptid);
 
-    printf("Main is running... \n");
+    cStaticTest *pTemp2 = new cStaticTest();
+    if (pTemp2)
+    {
+        delete pTemp2;
+        pTemp2 = NULL;
+    }
 
-    void *cRcv;
-    pthread_join(ptid, &cRcv);
-    printf("Thread over, and recv : %d\n", (*(STRTEMP*)cRcv).i);
-    delete cRcv;
-    cRcv = NULL;
+    cStaticTest *pTemp3 = new cStaticTest();
+    if (pTemp3)
+    {
+        delete pTemp3;
+        pTemp3 = NULL;
+    }
 
     system("pause");
 
-	return 0;
+    return 0;
 }
 
